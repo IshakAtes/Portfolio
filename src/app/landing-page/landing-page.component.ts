00e9ccd98696544dec67b975860b08d81247f25d
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-01-18 13:49:49 +0100

    added about-me component

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavBarComponent, HeaderComponent],
+  imports: [NavBarComponent, HeaderComponent, AboutMeComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
