7f8440d854a3e22ace0824a943c1287e1000241b
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-01-21 00:03:16 +0100

    added my-skills component

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavBarComponent, HeaderComponent, AboutMeComponent],
+  imports: [NavBarComponent, HeaderComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
