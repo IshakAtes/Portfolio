56b42a249db93da0b42eae5b8682c9bb5df914f3
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-01-21 22:45:59 +0100

    finished my skills section

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -3,11 +3,12 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioProjectsComponent } from './portfolio-projects/portfolio-projects.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavBarComponent, HeaderComponent, AboutMeComponent, MySkillsComponent],
+  imports: [NavBarComponent, HeaderComponent, AboutMeComponent, MySkillsComponent, PortfolioProjectsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
