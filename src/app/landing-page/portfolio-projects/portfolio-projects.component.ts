97e8a0b430aea20390215098375eec92643d021d
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-01-22 23:41:07 +0100

    added projects component

--- src/app/landing-page/portfolio-projects/portfolio-projects.component.ts
+++ src/app/landing-page/portfolio-projects/portfolio-projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-portfolio-projects',
   standalone: true,
-  imports: [],
+  imports: [ProjectsComponent],
   templateUrl: './portfolio-projects.component.html',
   styleUrl: './portfolio-projects.component.scss'
 })
