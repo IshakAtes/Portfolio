4647dbc29271c48508c1570c3bebbf07c55a70d3
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-02-04 00:49:37 +0100

    added form validation for email input

--- src/app/landing-page/contact/contact.component.ts
+++ src/app/landing-page/contact/contact.component.ts
@@ -1,11 +1,11 @@
-import { NgClass, NgIf } from '@angular/common';
+import { NgClass, NgIf, NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [NgClass, FormsModule, ReactiveFormsModule, NgIf],
+  imports: [NgClass, FormsModule, ReactiveFormsModule, NgIf, NgStyle,],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -13,5 +13,18 @@ export class ContactComponent {
   nameInput: string = '';
   minlength: number = 2;
   isButtonDisabled: boolean = true;
+  mailInput: string = '';
+  pattern: string = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+
+
+  constructor() {
+    setTimeout(()=> {
+      console.log(this.mailInput);
+    }, 4000);
+    // if (this.nameInput.length >= 2) {
+    //   console.log('input is Correct');
+    // }
+  }
+
 
 }
