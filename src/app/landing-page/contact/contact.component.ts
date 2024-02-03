bc9beeff5b0e206eff2f8646495c15073383c8f5
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-02-03 21:32:12 +0100

    add image and change border color if input.length greater then 2

--- src/app/landing-page/contact/contact.component.ts
+++ src/app/landing-page/contact/contact.component.ts
@@ -1,14 +1,17 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [NgClass, FormsModule, ReactiveFormsModule],
+  imports: [NgClass, FormsModule, ReactiveFormsModule, NgIf],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  nameInput: string = '';
+  minlength: number = 2;
+  isButtonDisabled: boolean = true;
 
 }
