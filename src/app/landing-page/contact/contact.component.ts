936afc29cd830bc1290e11ebd7e8cbad9fad7f02
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-02-05 11:59:09 +0100

    form works

--- src/app/landing-page/contact/contact.component.ts
+++ src/app/landing-page/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { NgClass, NgIf, NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, NgModel, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -10,22 +10,26 @@ import { FormsModule, NgModel, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-  nameInput: string = '';
+  myForm: FormGroup;
   textInput: string = '';
   minlength: number = 2;
   isButtonDisabled: boolean = true;
-  mailInput: string = '';
-  pattern: string = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
 
-
-  constructor() {
-    setTimeout(()=> {
-      console.log(this.mailInput);
-    }, 4000);
-    // if (this.nameInput.length >= 2) {
-    //   console.log('input is Correct');
-    // }
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      nameInput: ['', [Validators.required, Validators.minLength(2)]],
+      mailInput: ['', [Validators.required, Validators.email]],
+      text: ['', [Validators.required, Validators.minLength(6)]],
+      checkbox: [false, Validators.requiredTrue]
+    });
   }
 
+  isFormValid(): boolean {
+    return this.myForm.valid;
+  }
+
+  onSubmit() {
+    console.log('form works')
+  }
 
 }
