0b275b3ed6f6a2827360c094f7d560ad876388b5
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-02-19 13:40:00 +0100

    added mobile navBar

--- src/app/landing-page/nav-bar/nav-bar.component.ts
+++ src/app/landing-page/nav-bar/nav-bar.component.ts
@@ -1,12 +1,19 @@
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgStyle],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  constructor() {}
 
+  isActive: boolean = false; // Eine Variable, um den Zustand der Klasse zu verfolgen
+
+  toggleMenu() {
+    this.isActive = !this.isActive; // Umkehrung des Zustands beim Klicken auf den Button
+  }
 }
