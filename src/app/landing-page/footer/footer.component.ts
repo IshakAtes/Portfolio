f67d0b10239b2198cab5b64fda47a8107c1443d3
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-02-21 18:54:13 +0100

    added footer glitch on MouseOver

--- src/app/landing-page/footer/footer.component.ts
+++ src/app/landing-page/footer/footer.component.ts
@@ -1,14 +1,17 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  mouseOvered: boolean = false;
+  
   constructor(private router: Router) { }
 
   openImprint() {
