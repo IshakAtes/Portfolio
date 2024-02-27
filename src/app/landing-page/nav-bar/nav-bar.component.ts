e87a97979d1382999482a71cc666607be81d0480
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-02-27 13:51:50 +0100

    added angular internationalization

--- src/app/landing-page/nav-bar/nav-bar.component.ts
+++ src/app/landing-page/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,9 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
-  constructor() {}
+  title = 'Your Receipt';
+  constructor(private titleService: Title) {
+    this.titleService.setTitle($localize `${this.title}`);
+    console.log($localize, `${this.title}`);
+  }
 
   isActive: boolean = false; // Eine Variable, um den Zustand der Klasse zu verfolgen
+
+  switchLanguage(lang: string) {
+    // Setze den Titel entsprechend der ausgewählten Sprache
+    switch (lang) {
+      case 'en-US':
+        this.titleService.setTitle('English Title');
+        break;
+      case 'de-DE':
+        this.titleService.setTitle('German Title');
+        break;
+      // Weitere Sprachen hier hinzufügen...
+      default:
+        this.titleService.setTitle('Default Title');
+    }
+    console.log(lang, this.titleService.getTitle());
+  }
 
   toggleMenu() {
     this.isActive = !this.isActive; // Umkehrung des Zustands beim Klicken auf den Button
