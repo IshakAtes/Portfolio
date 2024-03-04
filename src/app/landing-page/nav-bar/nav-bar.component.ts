55b9b77eee6223925e7d4843ab6848aef45da824
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-03-04 12:34:43 +0100

    make Imprint and Privacy Policy Responsive

--- src/app/landing-page/nav-bar/nav-bar.component.ts
+++ src/app/landing-page/nav-bar/nav-bar.component.ts
@@ -20,22 +20,6 @@ export class NavBarComponent {
 
   isActive: boolean = false; // Eine Variable, um den Zustand der Klasse zu verfolgen
 
-  switchLanguage(lang: string) {
-    // Setze den Titel entsprechend der ausgewählten Sprache
-    switch (lang) {
-      case 'en-US':
-        this.titleService.setTitle('English Title');
-        break;
-      case 'de-DE':
-        this.titleService.setTitle('German Title');
-        break;
-      // Weitere Sprachen hier hinzufügen...
-      default:
-        this.titleService.setTitle('Default Title');
-    }
-    console.log(lang, this.titleService.getTitle());
-  }
-
   toggleMenu() {
     this.isActive = !this.isActive; // Umkehrung des Zustands beim Klicken auf den Button
   }
