0d90ff7482a83dd1ff029a491e9597c73e0dc738
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2024-05-20 21:32:35 +0200

    change mailadress

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'ishakfeuer@gmail.com';  
+            $recipient = 'contact@ishakates.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
